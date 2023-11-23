ee95524eaa9fe7e2af084c2baade8c4bbb235728
Daniel Donnelly, Jr. <fruitfulapproach@gmail.com>
2023-11-23 11:23:13 -0700

    added 'arrowbee-d87fe54ff0ef.herokuapp.com' to allowed hosts

--- arrowbee/arrowbee/settings.py
+++ arrowbee/arrowbee/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arrowbee-d87fe54ff0ef.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
